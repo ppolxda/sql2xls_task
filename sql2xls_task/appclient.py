9a8a35513987f0ef48ac0759f94d6b2ac4056e46
ppolxda <ppol850564@gmail.com>
2020-04-07 16:01:43 +0000

    add AppClient config

--- sql2xls_task/appclient.py
+++ sql2xls_task/appclient.py
@@ -16,7 +16,7 @@ class WebHttpError(Exception):
 
 class AppClient(object):
 
-    def __init__(self, host, project=None):
+    def __init__(self, host, project=None, **sessions):
         if not host or \
             not isinstance(host, str) or \
                 not (host.startswith('http://') or
@@ -28,9 +28,10 @@ class AppClient(object):
 
         self.host = host
         self.project = project
+        self.sessions = sessions
 
     async def add_task_sync(self, user, sql_url, sql, sql_parames,
-                            options, memo='', project=None):
+                            options, memo='', project=None, **kwargs):
         project = self.__get_project(project)
         url = self.host + '/export/task/{project}/{user}/add/sync'.format(
             project=project, user=user
@@ -49,14 +50,14 @@ class AppClient(object):
         task = Task(**data)
         task.check()
 
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url, json=data) as response:
+        async with aiohttp.ClientSession(**self.sessions) as session:
+            async with session.post(url, json=data, **kwargs) as response:
                 if response.status != 200:
                     raise WebHttpError(response.reason)
                 return await response.json()
 
     async def add_task_async(self, user, sql_url, sql, sql_parames,
-                             options, memo='', project=None):
+                             options, memo='', project=None, **kwargs):
         project = self.__get_project(project)
         url = self.host + '/export/task/{project}/{user}/add/async'.format(
             project=project, user=user
@@ -75,51 +76,51 @@ class AppClient(object):
         task = Task(**data)
         task.check()
 
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url, json=data) as response:
+        async with aiohttp.ClientSession(**self.sessions) as session:
+            async with session.post(url, json=data, **kwargs) as response:
                 if response.status != 200:
                     raise WebHttpError(response.reason)
                 return await response.json()
 
-    async def delete_task_all(self, user, project=None):
+    async def delete_task_all(self, user, project=None, **kwargs):
         project = self.__get_project(project)
         url = self.host + '/export/task/{project}/{user}/all'.format(
             project=project, user=user
         )
 
-        async with aiohttp.ClientSession() as session:
-            async with session.delete(url) as response:
+        async with aiohttp.ClientSession(**self.sessions) as session:
+            async with session.delete(url, **kwargs) as response:
                 if response.status != 200:
                     raise WebHttpError(response.reason)
                 return await response.json()
 
-    async def delete_task_one(self, user, taskid, project=None):
+    async def delete_task_one(self, user, taskid, project=None, **kwargs):
         project = self.__get_project(project)
         url = self.host + '/export/task/{project}/{user}/{taskid}'.format(
             project=project, user=user, taskid=taskid
         )
 
-        async with aiohttp.ClientSession() as session:
-            async with session.delete(url) as response:
+        async with aiohttp.ClientSession(**self.sessions) as session:
+            async with session.delete(url, **kwargs) as response:
                 if response.status != 200:
                     raise WebHttpError(response.reason)
                 return await response.json()
 
-    async def get_task_list(self, user, project=None):
+    async def get_task_list(self, user, project=None, **kwargs):
         project = self.__get_project(project)
         url = self.host + '/export/task/{project}/{user}/list'.format(
             project=project, user=user
         )
 
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
+        async with aiohttp.ClientSession(**self.sessions) as session:
+            async with session.get(url, **kwargs) as response:
                 if response.status != 200:
                     raise WebHttpError(response.reason)
                 return await response.json()
 
-    async def upload_file(self, url, buffer, filetype):
-        async with aiohttp.ClientSession() as session:
-            async with session.put(url, data=buffer) as response:
+    async def upload_file(self, url, buffer, filetype, **kwargs):
+        async with aiohttp.ClientSession(**self.sessions) as session:
+            async with session.put(url, data=buffer, **kwargs) as response:
                 if response.status == 200:
                     return
 
